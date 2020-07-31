f1dc42170c9a46457cd53a6e02913df146ecdb30
Keith Packard <keithp@keithp.com>
2020-07-31 14:19:22 -0700

    Port to freedom-metal-next
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- example-pwm.c
+++ example-pwm.c
@@ -4,7 +4,6 @@
 #include <metal/cpu.h>
 #include <metal/pwm.h>
 #include <metal/io.h>
-#include <metal/machine.h>
 #include <metal/uart.h>
 #include <stdio.h>
 #include <time.h>
@@ -20,21 +19,27 @@
     ;
 
  /* To enable PWM run in interrupt mode.*/
- //#define ENABLE_INTERRUPTS
+//#define ENABLE_INTERRUPTS
 
-struct metal_pwm *pwm;
+#ifdef metal_pwm_1
+#define pwm metal_pwm_1
+#else
+#ifdef metal_pwm_0
+#define pwm metal_pwm_0
+#endif
+#endif
+
 #ifdef ENABLE_INTERRUPTS
-struct metal_cpu *cpu;
-struct metal_interrupt *pwm_intr, *cpu_intr;
 
-void pwm_isr0(int pwm_id, void *data) {
+void metal_sifive_pwm0_source0_handler(void)
+{
     static unsigned int i = 0;
 
     /* Print interrupt ID and ISR run count
      * Note: Not recommended, but we have nothing else to do here.*/
-    printf("id:%u i:%u\n", pwm_id, i++);
+    printf("id:%u i:%u\n", 0, i++);
 
-    metal_pwm_clr_interrupt((struct metal_pwm*) data, 0);
+    metal_pwm_clear_interrupt(pwm, 0);
 }
 
 void pwm_isr1(int pwm_id, void *data) {
@@ -42,9 +47,9 @@ void pwm_isr1(int pwm_id, void *data) {
 
     /* Print interrupt ID and ISR run count
      * Note: Not recommended, but we have nothing else to do here.*/
-    printf("id:%u i:%u\n", pwm_id, i++);
+    printf("id:%u i:%u\n", 1, i++);
 
-    metal_pwm_clr_interrupt((struct metal_pwm*) data, 1);
+    metal_pwm_clear_interrupt(pwm, 1);
 }
 #endif
 
@@ -54,60 +59,8 @@ int main(void) {
     printf("%s %s \n", __DATE__, __TIME__);
     printf("PWM demo test..\n");
 
-    /* Get a PWM device instance */
-    pwm = metal_pwm_get_device(1);
+#ifdef pwm
 
-    if (pwm == NULL) {
-        printf("PWM not available \n");
-        return RET_NOK;
-    }
-
-#ifdef ENABLE_INTERRUPTS
-    int pwm_id0, pwm_id1;
-    int rc;
-
-    /* Lets get the CPU and and its interrupt */
-    cpu = metal_cpu_get(metal_cpu_get_current_hartid());
-    if (cpu == NULL) {
-        printf("CPU is null.\n");
-        return RET_NOK;
-    }
-
-    cpu_intr = metal_cpu_interrupt_controller(cpu);
-    if (cpu_intr == NULL) {
-        printf("CPU interrupt controller is null.\n");
-        return RET_NOK;
-    }
-    metal_interrupt_init(cpu_intr);
-
-    /* Initialize PWM interrupt controller */
-    pwm_intr = metal_pwm_interrupt_controller(pwm);
-    if (pwm_intr == NULL) {
-        printf("PWM interrupt controller is  null.\n");
-        return RET_NOK;
-    }
-    metal_interrupt_init(pwm_intr);
-
-    /* Get id, set interrupt priority, register handler */
-    pwm_id0 = metal_pwm_get_interrupt_id(pwm, 0);
-    metal_interrupt_set_priority(pwm_intr, pwm_id0, 2);
-    rc = metal_interrupt_register_handler(pwm_intr, pwm_id0, pwm_isr0, pwm);
-
-    if (rc != 0) {
-        printf("PWM interrupt handler 0 registration failed\n");
-        return RET_NOK;
-    }
-
-    /* Get id, set interrupt priority, register handler */
-    pwm_id1 = metal_pwm_get_interrupt_id(pwm, 1);
-    metal_interrupt_set_priority(pwm_intr, pwm_id1, 2);
-    rc = metal_interrupt_register_handler(pwm_intr, pwm_id1, pwm_isr1, pwm);
-
-    if (rc != 0) {
-        printf("PWM interrupt handler 1 registration failed\n");
-        return RET_NOK;
-    }
-#endif
     /* Enable PWM1 instance */
     metal_pwm_enable(pwm);
 
@@ -121,24 +74,7 @@ int main(void) {
     metal_pwm_trigger(pwm, 0, METAL_PWM_CONTINUOUS);
 
 #ifdef ENABLE_INTERRUPTS
-    /* Enable interrupts */
-    if (metal_interrupt_enable(cpu_intr, 0)) {
-        printf("CPU interrupt enable failed\n");
-        return RET_NOK;
-    }
-
-    if (metal_interrupt_enable(pwm_intr, pwm_id0)) {
-        printf("PWM0 interrupt enable failed\n");
-        return RET_NOK;
-    }
-
-    if (metal_interrupt_enable(pwm_intr, pwm_id1)) {
-        printf("PWM1 interrupt enable failed\n");
-        return RET_NOK;
-    }
-
     metal_pwm_cfg_interrupt(pwm, METAL_PWM_INTERRUPT_ENABLE);
-
 #else
     metal_pwm_set_duty(pwm, 2, 90, METAL_PWM_PHASE_CORRECT_DISABLE);
     metal_pwm_set_duty(pwm, 3, 20, METAL_PWM_PHASE_CORRECT_DISABLE);
@@ -151,4 +87,8 @@ int main(void) {
     }
 
     return RET_OK;
+#else
+    printf("PWM not present\n");
+    return RET_NOK;
+#endif
 }
